4a7107bcb71d13cd9111ed1e3f8c4aa18fb741b8
arshali2774 <arshali.763z@gmail.com>
2024-04-24 14:23:50 +0530

    user postaway done

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import userRouter from './src/features/user/routes/user.routes.js';
+import { errorHandlerMiddleware } from './src/middleware/error.middleware.js';
+import { invalidRoutesHandlerMiddleware } from './src/middleware/invalidRoute.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors());
+app.use(cookieParser());
+
+//paths
+app.use('/api/v1/users', userRouter);
+
+//invalid routes
+app.use(invalidRoutesHandlerMiddleware);
+
+//error handling
+app.use(errorHandlerMiddleware);
 
 app.listen(3000, () => {
   console.log('server listening on 3000');
